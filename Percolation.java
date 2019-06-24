aa47eee2eaacd0a7900193d2340df53d86c9847f
zkxd <zkxd@gwu.edu>
2019-06-24 18:18:16 -0400

    Initial commit

---
+++ Percolation.java
@@ -0,0 +1,14 @@
+/* *****************************************************************************
+ *  Name:
+ *  Date:
+ *  Description:
+ **************************************************************************** */
+
+public class Percolation {
+    public Percolation(int n){
+
+    }
+    public static void main(String[] args) {
+
+    }
+}
